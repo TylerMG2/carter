e3ee71f3e45b88a1b12012922c09f484633e31cf
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-02-29 18:58:25 +1100

    Implemented get_pano_in_country

--- cogs/Geoguessr/streetview.py
+++ cogs/Geoguessr/streetview.py
@@ -3,17 +3,16 @@ from .panorama import Panorama
 from shapely.geometry import Point
 from shapely.geometry import Polygon, MultiPolygon
 from geopandas import GeoDataFrame
-from geopandas import GeoSeries
+import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 # Streetview API urls
-STREETVIEW_SEARCH_URL = "https://maps.googleapis.com/maps/api/streetview/metadata?location={0:},{1:}&key={2:}&radius=1000"
+STREETVIEW_SEARCH_URL = "https://maps.googleapis.com/maps/api/streetview/metadata?location={0:},{1:}&key={2:}&radius=30000"
 
 # Load world_map
-#world_map = GeoDataFrame.from_file('./world_map.shp')
-#print(world_map)
+world_map = GeoDataFrame.from_file('./resources/countries.geojson')
 
 # Function to get panos at a lat, long
 async def get_closest_pano(lat, long) -> Panorama:
@@ -35,7 +34,26 @@ async def get_closest_pano(lat, long) -> Panorama:
     location = response['location']
     return Panorama(response['pano_id'], location["lat"], location["lng"], response['date'])
 
-# Function to check if a point is within a polygon
-def point_in_polygon(point: Point, polygon: Polygon | MultiPolygon) -> bool:
-    return point.within(polygon)
+# Function to get a panorama within a given country
+async def get_pano_in_country(country: str) -> Panorama:
+    country = world_map[world_map['iso3'] == country]
+    country_poly : Polygon | MultiPolygon = country.geometry.iloc[0]
+    
+    # Run until we find a pano
+    pano = None
+    while not pano:
 
+        # Get point within the country
+        point = country_poly.point_on_surface()
+        pano = await get_closest_pano(point.y, point.x)
+
+        # Time out for a bit
+        await asyncio.sleep(0.2)
+
+        # Check that panorama is within the country
+        if pano is not None:
+            pano_point = Point(pano.long, pano.lat)
+            if not pano_point.intersects(country_poly):
+                pano = None
+    
+    return pano
--- cogs/Geoguessr/tests/test_geoguessr.py
+++ cogs/Geoguessr/tests/test_geoguessr.py
@@ -1,11 +1,8 @@
 import pytest
-from shapely.geometry import Point, Polygon, MultiPolygon
-from geopandas import GeoSeries
 from ..panorama import Panorama
-from ..streetview import get_closest_pano, point_in_polygon
+from ..streetview import get_closest_pano, get_pano_in_country
 
 KNOWN_PANO_LOCATION = (40.7128, -74.0060)
-POLYGON = Polygon(((0, 0), (1, 0), (1, 1), (0, 1)))
 
 # Test that the get_closest_pano function returns a Panorama object
 @pytest.mark.asyncio
@@ -32,45 +29,12 @@ async def test_get_closest_pano_close():
     assert abs(pano.lat - KNOWN_PANO_LOCATION[0]) < 0.1
     assert abs(pano.long - KNOWN_PANO_LOCATION[1]) < 0.1
 
-## Tests for point_in_polygon(point: Point, polygon: Polygon | MultiPolygon)
-    
-# Test that point_in_polygon returns True when a point is within a polygon
-def test_point_in_polygon_valid():
-    point = Point(0, 0)
-    assert point_in_polygon(point, POLYGON)
-
-# Test that point_in_polygon returns False when a point is not within a polygon
-def test_point_in_polygon_invalid():
-    point = Point(2, 2)
-    assert not point_in_polygon(point, POLYGON)
-
-# Test with a MultiPolygon
-def test_point_in_multipolygon():
-    point = Point(0, 0)
-    multipolygon = MultiPolygon([POLYGON])
-    assert point_in_polygon(point, multipolygon)
-
-## Tests for get_point_in_polygon(polygon: Polygon | MultiPolygon)
-
-# Test that get_point_in_polygon returns a Point object
-def test_get_point_in_polygon():
-    point = get_point_in_polygon(POLYGON)
-    assert point is not None
-    assert isinstance(point, Point)
-    assert point.x is not None
-    assert point.y is not None
-
-# Test that get_point_in_polygon returns a point within the polygon
-def test_get_point_in_polygon_valid():
-    point = get_point_in_polygon(POLYGON)
-    assert point_in_polygon(point, POLYGON)
-
 ## Tests for get_pano(country: str)
     
 # Test that get_pano returns a Panorama object
 @pytest.mark.asyncio
 async def test_get_pano():
-    pano = await get_pano("us")
+    pano = await get_pano_in_country("AUS")
     assert pano is not None
     assert isinstance(pano, Panorama)
     assert pano.pano_id is not None
