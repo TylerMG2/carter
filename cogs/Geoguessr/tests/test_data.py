4cecd6f3ea0fda368ce7079bc3bbd620f7311b5a
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-03-01 21:38:27 +1100

    Added test for missing countries

---
+++ cogs/Geoguessr/tests/test_data.py
@@ -0,0 +1,28 @@
+## Load country_data.csv
+import pandas as pd
+import geopandas as gpd
+from ..streetview import get_pano_in_country
+import asyncio
+import pytest
+
+# Load country data
+country_data = pd.read_csv('./resources/country_data.csv')
+
+# Load world map
+world = gpd.read_file('./resources/country_shapes.geojson')
+
+# Test that all the countries in the country_data.csv file are in the world map
+def test_country_data():
+    for country in country_data['alpha-3']:
+        assert country in world['iso3'].values
+
+# Test that get_pano_in_country returns a valid panorama for each country in under 5 seconds
+@pytest.mark.asyncio
+async def test_get_pano_in_country():
+    for country in country_data['alpha-3']:
+        try:
+            pano = await asyncio.wait_for(get_pano_in_country(country), timeout=5)
+            assert pano is not None
+        except asyncio.TimeoutError:
+            print(f"Timeout for {country}")
+            assert False
