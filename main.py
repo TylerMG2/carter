9e8be0fdeeba30316e2284416216ba531dd0e22d
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-03-01 18:56:01 +1100

    Fixed issue with command syncing

--- main.py
+++ main.py
@@ -19,10 +19,18 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+    # Set status
+    await bot.change_presence(activity=discord.Game(name="/challenge"))
+
     # Load cogs
     for folder in os.listdir('./cogs'):
         if os.path.isdir(f'./cogs/{folder}') and not folder.startswith('_'):
             await bot.load_extension(f'cogs.{folder}.cog')
+    
+    # Sync commands
+    if os.getenv("ENV") == "production":
+        await sync_commands()
 
 # Ping command
 @bot.tree.command(name='ping', description='Replies with Pong!')
@@ -35,16 +43,17 @@ async def sync_commands(guild: Guild = None):
     # If guild was supplied, copy global commands to guild
     if guild is not None:
         bot.tree.copy_global_to(guild=guild)
-
-    # Sync commands
-    commands = await bot.tree.sync(guild=guild)
-    print(f"Synced {len(commands)} commands")
+        commands = await bot.tree.sync(guild=guild)
+        print(f"Synced {len(commands)} commands to {guild.name}")
+    else:
+        commands = await bot.tree.sync()
+        print(f"Synced {len(commands)} commands")
 
 # Normal command to sync slash commands to current guild
 @bot.command()
 @commands.is_owner()
 async def sync(ctx: Context):
-    if os.getenv("ENV") != "production":
+    if os.getenv("ENV") == "development":
         await sync_commands(guild=ctx.guild)
         await ctx.send('Synced commands to this server!')
 
