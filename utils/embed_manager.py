af59bb4cea28902b450228752a6ef2f0f3115472
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-03-07 19:53:07 +1100

    Refactor EmbedManager class and add new methods for updating author and embed

--- utils/embed_manager.py
+++ utils/embed_manager.py
@@ -1,12 +1,14 @@
-from discord import Embed, Message, User
+from datetime import datetime
+from typing import Any
+from discord import Colour, Embed, Message, User
 from discord.ext import commands
 
 # This class is used to manage the creation, editing and deletion of embeds
 #TODO: Add some errors for when the message_id or channel_id is not set
-class EmbedManager:
+class EmbedMessage(Embed):
 
     """
-    A class used to manage a single embed message.
+    A extension class of discordpy Embed used to manage a single embed message.
     Call update after editing the embed message to update the message.
 
     ...
@@ -40,84 +42,80 @@ class EmbedManager:
         Deletes the embed message
     
     """
-    FOOTER_TEST = "Bot by @tmg1"
+    FOOTER_TEXT = "Bot by @tmg1"
 
     # Constructor
-    def __init__(self, bot: commands.Bot, embed: Embed=None):
+    def __init__(self, bot: commands.Bot):
+        super().__init__()
         self.bot = bot
-        self.embed = embed
-        self.author_id : int = None
-        self.message_id : int = None
-        self.channel_id : int = None
+        self.author_id = None
+        self.message_id = None
+        self.channel_id = None
 
     # Function to get the embed message
     async def get_message(self) -> Message:
+
+        # Validate message and channel id
         if self.message_id is None or self.channel_id is None:
-            return None
+            raise ValueError("Message or channel id not set")
         channel = self.bot.get_channel(self.channel_id)
+
+        # Validate channel
+        if channel is None:
+            raise ValueError("Channel not found")
+
         return await channel.fetch_message(self.message_id)
     
     # Function to get the author of the embed message
-    async def get_author(self) -> User:
+    async def _get_author(self) -> User:
         if self.author_id is None:
             return None
-        return await self.bot.fetch_user(self.author_id)
+        author = await self.bot.fetch_user(self.author_id)
+        return author
+    
+    # Function to update the author of the embed message
+    async def update_author(self, author_id: int) -> None:
 
-    # Function to create an embed
-    async def create(self, title: str, description: str, color: int = 0x00ff00, author_id: int = None) -> Embed:
-        self.embed = Embed(title=title, description=description, color=color)
+        # No author, set bot as the author
+        if author_id is None:
+            self.set_author(name=self.bot.user.display_name, icon_url=self.bot.user.avatar.url)
+            return
+        
+        # Update the author_id and the embed message
+        self.author_id = author_id
+        author = await self._get_author()
+        if not author:
+            raise ValueError("Author not found")
+        self.set_author(name=author.display_name, icon_url=author.avatar.url)
+    
+    # Function to reset current embed values and set new valies
+    async def update_embed(self, title: str, description: str, color: int = 0x00ff00) -> Embed:
+        self.clear_fields()
+        self.title = title
+        self.description = description
+        self.color = color
 
-        # Set author
-        if author_id:
-            self.author_id = author_id
-            author = await self.get_author()
-            self.embed.set_author(name=author.display_name, icon_url=author.avatar.url)
-        else:
-            self.embed.set_author(name=self.bot.user.display_name, icon_url=self.bot.user.avatar.url)
-
-        self.embed.set_footer(text=self.FOOTER_TEST)
-        return self.embed
+        # Author and footer
+        await self.update_author(self.author_id)
+        self.set_footer(text=self.FOOTER_TEXT)
+        return self
     
     # Function to send an embed
-    async def send(self, channel_id: int) -> None:
+    async def send(self, channel_id: int) -> Message:
         channel = self.bot.get_channel(channel_id)
-        message = await channel.send(embed=self.embed)
+        if channel is None:
+            raise ValueError("Channel not found")
+        message = await channel.send(embed=self)
         self.message_id = message.id
         self.channel_id = channel_id
+        return message
 
-    # Function to update an embed
-    async def update(self, embed: Embed = None) -> None:
-        if embed:
-            self.embed = embed
+    # Function to update the embeds message
+    async def update(self) -> None:
         message = await self.get_message()
-        if message:
-            await message.edit(embed=self.embed)
+        await message.edit(embed=self)
 
     # Function to delete an embed
     async def delete(self) -> None:
         message = await self.get_message()
-        if message:
-            await message.delete()
-
-
-class EmbedMessage(Embed):
-
-    def __init__(self, title: str, description: str, color: int = 0x00ff00, author_id: int = None):
-        super().__init__(title=title, description=description, color=color)
-        self.author_id = author_id
-        self.set_footer(text=EmbedManager.FOOTER_TEST)
-
-    async def send(self, channel_id: int, bot: commands.Bot) -> None:
-        channel = bot.get_channel(channel_id)
-        message = await channel.send(embed=self)
-        return message.id
-
-    async def update(self, message_id: int, channel_id: int, bot: commands.Bot) -> None:
-        channel = bot.get_channel(channel_id)
-        message = await channel.fetch_message(message_id)
-        await message.edit(embed=self)
-
-    async def delete(self, message_id: int, channel_id: int, bot: commands.Bot) -> None:
-        channel = bot.get_channel(channel_id)
-        message = await channel.fetch_message(message_id)
         await message.delete()
